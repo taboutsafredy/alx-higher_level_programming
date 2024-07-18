67cc2e274e6d93de4bc45ffb369bf265eea21ef5
It's Fredy <taboutsafredy@gmail.com>
2024-07-18 03:53:37 +0000

    clear empty

--- 0x10-python-network_0/1-body.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-
-# -s = Silent mode. Don't show progress meter or error messages.
-# -L = Follow redirects in URL
-curl -sL "$1"
--- 0x10-python-network_0/101-post_json.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Sends a JSON POST request to a given URL with a given JSON file.
-
-# -s = Silent mode. Don't show progress meter or error messages.
-# -H = Specify header content type to be application/json.
-# -d = Specify HTTP POST data to post.
-# cat "$2" = Read the contents of the JSON file.
-
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
